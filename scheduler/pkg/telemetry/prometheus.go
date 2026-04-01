1e33622b98727f6ad6896fecce1aec3404c2e6e7
noahdavis05 <sc23nd@leeds.sc.uk>
2026-04-01 22:25:04 +0100

    Longer telemetry period used

--- scheduler/pkg/telemetry/prometheus.go
+++ scheduler/pkg/telemetry/prometheus.go
@@ -240,12 +240,12 @@ func nodeTelemetryAll(prompApi promv1.API, nodeIP string, seconds int64) types.N
 
 func requestNodeTelemetry(promApi promv1.API) map[string]types.NodeTelemetryMetrics {
 	queries := []telemetryQuery{
-		{`avg by (instance) (100 - (rate(node_cpu_seconds_total{mode="idle"}[1m]) * 100))`, "CPU", "Mean"},
-		{`min_over_time((100 - (avg by (instance) (rate(node_cpu_seconds_total{mode="idle"}[1m])) * 100))[1m:15s])`, "CPU", "Low"},
-		{`max_over_time((100 - (avg by (instance) (rate(node_cpu_seconds_total{mode="idle"}[1m])) * 100))[1m:15s])`, "CPU", "High"},
-		{`100 * (1 - (node_memory_MemAvailable_bytes / node_memory_MemTotal_bytes))`, "RAM", "Mean"},
-		{`min_over_time((100 * (1 - (node_memory_MemAvailable_bytes / node_memory_MemTotal_bytes)))[1m:15s])`, "RAM", "Low"},
-		{`max_over_time((100 * (1 - (node_memory_MemAvailable_bytes / node_memory_MemTotal_bytes)))[1m:15s])`, "RAM", "High"},
+		{`avg by (instance) (100 - (rate(node_cpu_seconds_total{mode="idle"}[3m]) * 100))`, "CPU", "Mean"},
+		{`min_over_time((100 - (avg by (instance) (rate(node_cpu_seconds_total{mode="idle"}[1m])) * 100))[3m:1m])`, "CPU", "Low"},
+		{`max_over_time((100 - (avg by (instance) (rate(node_cpu_seconds_total{mode="idle"}[1m])) * 100))[3m:1m])`, "CPU", "High"},
+		{`avg_over_time((100 * (1 - (node_memory_MemAvailable_bytes / node_memory_MemTotal_bytes)))[3m:1m])`, "RAM", "Mean"},
+		{`min_over_time((100 * (1 - (node_memory_MemAvailable_bytes / node_memory_MemTotal_bytes)))[3m:1m])`, "RAM", "Low"},
+		{`max_over_time((100 * (1 - (node_memory_MemAvailable_bytes / node_memory_MemTotal_bytes)))[3m:1m])`, "RAM", "High"},
 	}
 
 	// make a results map
