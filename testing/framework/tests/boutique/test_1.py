f27874c797db92d5d0018835359634c52a008e5e
noahdavis05 <sc23nd@leeds.sc.uk>
2026-04-02 12:54:13 +0100

    Changed the test so I run locust locally instead

--- testing/framework/tests/boutique/test_1.py
+++ testing/framework/tests/boutique/test_1.py
@@ -12,20 +12,6 @@ def boutique_load_test(scheduler_name, save_dir):
     # deploy the whole online boutique application
     deploy_online_boutique(scheduler_name)
 
-    # sleep for 1 min while initial deployment is deployed.
-    time.sleep(50)
-
-    # start load testing using the locust script
-    locust_command = [
-        "locust",
-        "-f", locust_file,      
-        "--headless",           
-        "-u", "500",            
-        "-r", "2",            
-        "--run-time", "5m",     
-        "--host", "http://192.168.0.200/", 
-        "--csv", save_dir + "/results/" + scheduler_name + "-results-boutique"
-    ]
-
-    print("Starting Locust Load Test...")
-    subprocess.run(locust_command)
+    # sleep for 7 min while initial deployment is deployed.
+    # and locust test is manually carried out.
+    time.sleep(420)
